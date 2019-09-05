5ce8791aee1d0fd0904f2d760b31aac0f8b77277
Wendy Hamilton <wdhamilton117@gmail.com>
2019-09-05 17:36:17 +0000

    filled out cli file

--- lib/ncaa_champs/cli.rb
+++ lib/ncaa_champs/cli.rb
@@ -1,10 +1,55 @@
+#CLI CONTROLLER
 class NcaaChamps::CLI 
   
-  def call
-    puts "NCAA Champions!"
+  def call  
+    puts "NCAA Champs"
+    list_intro
+    menu
   end
   
+  def list_intro
+    puts <<-DOC
+    Welcome to the NCAA Champions Information Center
+    DOC
+  end
   
+  def menu
+    input = nil 
+    while input != "exit"
+    puts "Enter a year to find out which team won the NCAA Championship! (or enter exit to leave)"
+    input = gets.strip.downcase
+    if input == "2019"
+      puts "Virginia"
+      more_information
+    elsif input == "2018"
+      puts "Villanova"
+      more_information
+    elsif input == "2017"
+      puts "North Carolina"
+      more_information
+    elsif input == "exit"
+      puts "Ok see ya!"
+    else
+      puts "We don't have information on that year yet, try back soon!"
+    end
+  end
+end
+  
+  def more_information
+    puts "Enter 1 to view final score; Enter 2 to view location; Enter 3 to view opponent; Enter 4 to select a new year; Enter exit to leave"
+    input = gets.strip
+    if input == "1"
+      puts "Final Score is *"
+    elsif input == "2"
+      puts "Location: Somewhere"
+    elsif input == "3"
+      puts "opponent is you!"
+    elsif input == "4"
+    menu
+    elsif input == "exit"
+    puts "Goodbye! See you next time."
+  end
+end
   
   
 end
