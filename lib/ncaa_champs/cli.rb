93b761f485a4a22928b90e68f8158d444a0f0b08
Wendy Hamilton <wdhamilton117@gmail.com>
2019-09-13 14:20:36 +0000

    added list option to menu

--- lib/ncaa_champs/cli.rb
+++ lib/ncaa_champs/cli.rb
@@ -21,7 +21,8 @@ class NcaaChamps::CLI
   def menu
     input = nil 
     while input != "exit"
-    puts "Select an option from the list above to see who won and more!(or exit to leave)"
+    puts "Select option from the list above to see who won and more!"
+    puts "(Enter exit to leave or list to view options again)"
     input = gets.strip.downcase
     if input.to_i > 4
       puts "Invalid entry. Please enter your selection. (1-4)"
@@ -32,6 +33,8 @@ class NcaaChamps::CLI
       puts "Final score was #{the_champs.score}\n\n"
     elsif input == "exit"
     puts "Thanks for stopping by!"
+  elsif input == "list"
+    list_champs
   else 
     puts "Invalid entry. please try again. Please enter your selection. (1-4)"
     end
