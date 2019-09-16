5341c332756b000d8134e6f1eb58192a57aaeac6
Wendy Hamilton <wdhamilton117@gmail.com>
2019-09-16 03:01:49 +0000

    finished champs.rb structure

--- lib/ncaa_champs/champs.rb
+++ lib/ncaa_champs/champs.rb
@@ -8,7 +8,7 @@ class NcaaChamps::Champs
   end
   
   def self.scrape_champs
-     champs = []
+    champs = []
     
     champs << self.scrape_champs_1
     champs << self.scrape_champs_2
@@ -20,38 +20,49 @@ class NcaaChamps::Champs
   
   def self.scrape_champs_1
     doc = Nokogiri::HTML(open("https://www.ncaa.com/history/basketball-men/d1"))
-    year = doc.css("div.node__content td").children[0]
-    name = doc.css("div.node__content td a").children[0]
-    opponent = doc.css("div.node__content td").children[4]
-    score = doc.css("div.node__content td").children[3]
     
+    champ = self.new
+    champ.year = doc.css("div.node__content td").children[0]
+    champ.name = doc.css("div.node__content td a").children[0]
+    champ.opponent = doc.css("div.node__content td").children[4]
+    champ.score = doc.css("div.node__content td").children[3]
+    champ
   end
 
   def self.scrape_champs_2
     doc = Nokogiri::HTML(open("https://www.ncaa.com/history/basketball-men/d1"))
-    year = doc.css("div.node__content td").children[6]
-    name = doc.css("div.node__content td a").children[1]
-    opponent = doc.css("div.node__content td").children[10]
-    score = doc.css("div.node__content td").children[9]
     
+    champ = self.new
+    champ.year = doc.css("div.node__content td").children[6]
+    champ.name = doc.css("div.node__content td a").children[1]
+    champ.opponent = doc.css("div.node__content td").children[10]
+    champ.score = doc.css("div.node__content td").children[9]
+   
+    champ
   end
 
   def self.scrape_champs_3
     doc = Nokogiri::HTML(open("https://www.ncaa.com/history/basketball-men/d1"))
-    year = doc.css("div.node__content td").children[12]
-    name = doc.css("div.node__content td a").children[2]
-    opponent = doc.css("div.node__content td").children[16]
-    score = doc.css("div.node__content td").children[15]
     
+    champ = self.new
+    champ.year = doc.css("div.node__content td").children[12]
+    champ.name = doc.css("div.node__content td a").children[2]
+    champ.opponent = doc.css("div.node__content td").children[16]
+    champ.score = doc.css("div.node__content td").children[15]
+   
+    champ
   end
 
   def self.scrape_champs_4
     doc = Nokogiri::HTML(open("https://www.ncaa.com/history/basketball-men/d1"))
-    year = doc.css("div.node__content td").children[18]
-    name = doc.css("div.node__content td a").children[3]
-    opponent = doc.css("div.node__content td").children[22]
-    score = doc.css("div.node__content td").children[21]
     
+    champ = self.new
+    champ.year = doc.css("div.node__content td").children[18]
+    champ.name = doc.css("div.node__content td a").children[3]
+    champ.opponent = doc.css("div.node__content td").children[22]
+    champ.score = doc.css("div.node__content td").children[21]
+    
+    champ
   end
   
 end
